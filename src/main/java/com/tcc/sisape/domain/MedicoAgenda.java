af2a75d3d4fc742234b7e646bae86a1d11e743b4
Marcos Demetrio <demetrius.marcos@gmail.com>
2016-08-14 17:52:22 -0300

    id

--- src/main/java/com/tcc/sisape/domain/MedicoAgenda.java
+++ src/main/java/com/tcc/sisape/domain/MedicoAgenda.java
@@ -5,27 +5,14 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-
-import com.tcc.sisape.domain.enumdados.DiaSemana;
-import com.tcc.sisape.domain.enumdados.PeriodoDia;
 
 @Entity
 public class MedicoAgenda {
 	
 	@EmbeddedId
 	private MedicoAgendaId id;
-	
-	@Column(nullable = false)
-	@Enumerated(EnumType.ORDINAL)
-	private DiaSemana diaSemana;
-	
-	@Column(nullable = false)
-	@Enumerated(EnumType.ORDINAL)
-	private PeriodoDia periodoDia;
 	
 	@Column(nullable = false)
 	@Temporal(TemporalType.TIME)
--- src/main/java/com/tcc/sisape/domain/MedicoAgendaId.java
+++ src/main/java/com/tcc/sisape/domain/MedicoAgendaId.java
@@ -4,34 +4,30 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinColumns;
 import javax.persistence.ManyToOne;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.tcc.sisape.domain.enumdados.DiaSemana;
+import com.tcc.sisape.domain.enumdados.PeriodoDia;
 
 @Embeddable
 public class MedicoAgendaId implements Serializable{
 
 	private static final long serialVersionUID = 2664949603815082160L;
-
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@JsonProperty("i_medico_agenda")
-	@Column(name = "i_medico_agenda")
-	private Long id;
-		
+	
 	@ManyToOne(optional = false)
 	@JoinColumns(value = {@JoinColumn(name = "i_medico", referencedColumnName = "i_medico", nullable = false),
 						  @JoinColumn(name = "i_unidade_basica_saude", referencedColumnName = "i_unidade_basica_saude", nullable = false)})
 	private MedicoParametro medicoParametro;
 
-	public Long getId() {
-		return this.id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
+	@Column(nullable = false)
+	@Enumerated(EnumType.ORDINAL)
+	private DiaSemana diaSemana;
+	
+	@Column(nullable = false)
+	@Enumerated(EnumType.ORDINAL)
+	private PeriodoDia periodoDia;
 }
