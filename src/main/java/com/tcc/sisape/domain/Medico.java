12b732b9384d9d4eb53443440666d654f7cfc4cd
Marcos Demetrio <demetrius.marcos@gmail.com>
2016-08-16 19:57:40 -0300

    ident

--- src/main/java/com/tcc/sisape/domain/Medico.java
+++ src/main/java/com/tcc/sisape/domain/Medico.java
@@ -24,73 +24,73 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
 public class Medico {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@JsonProperty("i_medico")
 	@JsonInclude(Include.NON_NULL)
 	@Column(name = "i_medico")
 	private Long id;
-	
+
 	@JsonInclude(Include.NON_EMPTY)
 	@Column(length = 255, nullable = false)
 	private String nome;
-	
+
 	@CPF
 	@JsonInclude(Include.NON_EMPTY)
 	@Column(length = 11, nullable = false)
 	private String cpf;
-	
-	//private String cns;//duvida
-	
+
+	// private String cns;//duvida
+
 	@Temporal(TemporalType.DATE)
 	private Date dataNascimento;
-	
-	@Enumerated(EnumType.STRING)//, columnDefinition = "char(1) default 'M'"
+
+	@Enumerated(EnumType.STRING) // , columnDefinition = "char(1) default 'M'"
 	@Column(length = 1, nullable = false)
 	private Sexo sexo;
-	
-	//private String conselhoClasse;//duvida
-	//private String orgaoEmissor;//duvida
-	//private String registroConselhoClasse;//duvida
-	
+
+	// private String conselhoClasse;//duvida
+	// private String orgaoEmissor;//duvida
+	// private String registroConselhoClasse;//duvida
+
 	@Email
 	@Column(length = 255, nullable = false)
 	private String email;
-	
+
 	@JsonInclude(Include.NON_EMPTY)
 	@Column(length = 12)
 	private String telefonePrincipal;
-	
+
 	@JsonInclude(Include.NON_EMPTY)
 	@Column(length = 12)
 	private String telefoneSecundario;
-	
+
 	@Column(length = 8, nullable = false)
 	private String cep;
-	
+
 	@ManyToOne(optional = false)
 	@JoinColumn(name = "i_cidade", referencedColumnName = "i_cidade", nullable = false)
 	private Cidade cidade;
 
 	@Column(length = 255, nullable = false)
 	private String bairro;
-	
+
 	@Column(length = 255, nullable = false)
 	private String logradouro;
-	
+
 	@JsonInclude(Include.NON_EMPTY)
 	private Long numero;
 
 	@Column(nullable = false)
 	private boolean semNumero;
-	
+
 	@Column(length = 255, nullable = false)
 	private String complemento;
-	
+
 	@Column(length = 255, nullable = false)
 	private String pontoReferencia;
-	
+
 	public Long getId() {
 		return this.id;
 	}
@@ -106,7 +106,7 @@ public class Medico {
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
-	
+
 	public String getCpf() {
 		return this.cpf;
 	}
--- src/main/java/com/tcc/sisape/domain/MedicoAgenda.java
+++ src/main/java/com/tcc/sisape/domain/MedicoAgenda.java
@@ -18,32 +18,32 @@ import com.tcc.sisape.domain.enumdados.DiaSemana;
 import com.tcc.sisape.domain.enumdados.PeriodoDia;
 
 @Entity
-public class MedicoAgenda implements Serializable{
-	
+public class MedicoAgenda implements Serializable {
+
 	private static final long serialVersionUID = -1149180357096558979L;
 
 	@Id
 	@ManyToOne(optional = false)
-	@JoinColumns(value = {@JoinColumn(name = "i_medico", referencedColumnName = "i_medico", nullable = false),
-						  @JoinColumn(name = "i_unidade_basica_saude", referencedColumnName = "i_unidade_basica_saude", nullable = false)})
+	@JoinColumns(value = { @JoinColumn(name = "i_medico", referencedColumnName = "i_medico", nullable = false),
+			@JoinColumn(name = "i_unidade_basica_saude", referencedColumnName = "i_unidade_basica_saude", nullable = false) })
 	private MedicoParametro medicoParametro;
 
 	@Id
 	@Column(nullable = false)
 	@Enumerated(EnumType.ORDINAL)
 	private DiaSemana diaSemana;
-	
+
 	@Id
 	@Column(nullable = false)
 	@Enumerated(EnumType.ORDINAL)
 	private PeriodoDia periodoDia;
-	
+
 	@Column(nullable = false)
 	@Temporal(TemporalType.TIME)
 	private Date horarioInicio;
-	
+
 	@Column(nullable = false)
 	@Temporal(TemporalType.TIME)
 	private Date horarioFim;
-	
+
 }
