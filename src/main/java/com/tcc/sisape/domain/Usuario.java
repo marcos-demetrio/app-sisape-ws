04a43e35bf6583c8cc6dc7a07b963115e28ab4cb
Marcos Demetrio <demetrius.marcos@gmail.com>
2016-08-27 19:25:43 -0300

    Remover import não usados

--- src/main/java/com/tcc/sisape/domain/Usuario.java
+++ src/main/java/com/tcc/sisape/domain/Usuario.java
@@ -1,10 +1,7 @@
 package com.tcc.sisape.domain;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
--- src/main/java/com/tcc/sisape/service/SegurancaServiceImpl.java
+++ src/main/java/com/tcc/sisape/service/SegurancaServiceImpl.java
@@ -3,7 +3,6 @@ package com.tcc.sisape.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.tcc.sisape.domain.Usuario;
 import com.tcc.sisape.repository.UsuariosRepository;
 
 @Service("segurancaService")
