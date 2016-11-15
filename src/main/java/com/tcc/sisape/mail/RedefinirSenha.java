32fe2eacf7cf625c3da06c3a84f48aa3562d857e
Marcos Demetrio Nascimento <demetrius.marcos@gmail.com>
2016-11-14 22:21:49 -0200

    Ajuste Mail

--- src/main/java/com/tcc/sisape/mail/RedefinirSenha.java
+++ src/main/java/com/tcc/sisape/mail/RedefinirSenha.java
@@ -7,19 +7,42 @@ public class RedefinirSenha {
 
 	private ApplicationContext context;
 
-	private void enviarEmail() {
+	private void enviarEmail(String aEmail, String aNovaSenha) {
 		context = new ClassPathXmlApplicationContext("redefinir-senha-email-bean.xml");
 
-		String toAddress = "demetrius.marcos@gmail.com";
-		String fromAddress = "demetrius.marcos@gmail.com";
+		// Email: sisape.tcc@gmail.com
+		// Senha: sisapeTCC
+
+		String toAddress = aEmail;
+		String fromAddress = "sisape.tcc@gmail.com";
 		String subject = "Nova senha - SISAPE";
-		String msgBody = "Sua nova senha no SISPE é: XYZ";
+		String msgBody = "Sua nova senha no SISAPE é: " + aNovaSenha;
 
 		Mail mensagem = (Mail) context.getBean("mailSenha");
 		mensagem.sendMail(fromAddress, toAddress, subject, msgBody);
 	}
 
 	public void novaSenha(String toAddress) {
-		enviarEmail();
+		String senha = this.gerarSenhaAleatoria();
+
+		this.enviarEmail(toAddress, senha);
+	}
+
+	private String gerarSenhaAleatoria() {
+		int qtdeMaximaCaracteres = 8;
+		String[] caracteres = { "a", "1", "b", "2", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g",
+				"h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B",
+				"C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W",
+				"X", "Y", "Z" };
+
+		StringBuilder senha = new StringBuilder();
+
+		for (int i = 0; i < qtdeMaximaCaracteres; i++) {
+			int posicao = (int) (Math.random() * caracteres.length);
+
+			senha.append(caracteres[posicao]);
+		}
+
+		return senha.toString();
 	}
 }
--- src/main/java/com/tcc/sisape/resource/UsuarioResource.java
+++ src/main/java/com/tcc/sisape/resource/UsuarioResource.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,9 +23,8 @@ public class UsuarioResource {
 	@Autowired
 	private UsuarioService usuarioService;
 
-	@RequestMapping(value = "/novasenha", method = RequestMethod.GET)
-	public ResponseEntity<Void> novaSenha(@RequestParam(value = "email", defaultValue = "") String aEmail) {
-
+	@RequestMapping(value = "/novasenha/{email}", method = RequestMethod.GET)
+	public ResponseEntity<Void> novaSenha(@PathVariable("email") String aEmail) {
 		RedefinirSenha redefinirSenha = new RedefinirSenha();
 		redefinirSenha.novaSenha(aEmail);
 
